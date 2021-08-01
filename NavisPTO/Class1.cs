dc72398d3362d87d345f37f1d58c0351fb096020
romanvladi <romanvladi2@gmail.com>
2021-08-01 12:47:10 +0300

    Добавьте файлы проекта.

---
+++ NavisPTO/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavisPTO
+{
+    public class Class1
+    {
+    }
+}
